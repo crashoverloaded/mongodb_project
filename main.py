593493ecda956674e4f405d358e6362e8702574d
Priyank agarwa <crashoverload@pop-os.localdomain>
2022-12-29 12:23:50 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pymongo
+
+# Connecting to mongodb
+client = pymongo.MongoClient("mongodb+srv://crashoverload:manu2000@priyank.quin4el.mongodb.net/?retryWrites=true&w=majority")
+
+# testing connection
+db = client.test
+print(db)
+
+d = {
+    "name":"priyank",
+      "surname":"agarwal",
+      "email_id": "priyank@gmail.com"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
